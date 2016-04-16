2a9c249110f4d37a33ed89a84c222f19dd6bbfbd
chenjiai <170863190@qq.com>
2016-04-16 16:07:36 +0800

    app init

---
+++ doc/security.sql
@@ -0,0 +1,135 @@
+-- MySQL dump 10.13  Distrib 5.6.20, for Win32 (x86)
+--
+-- Host: localhost    Database: security
+-- ------------------------------------------------------
+-- Server version	5.6.20
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE `security` DEFAULT CHARACTER SET utf8 ;
+--
+-- Table structure for table `t_auth`
+--
+
+DROP TABLE IF EXISTS `t_auth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_auth` (
+  `id_AUTH` int(11) NOT NULL AUTO_INCREMENT,
+  `USER_ID` int(11) NOT NULL,
+  `AUTH_TYPE` char(1) NOT NULL,
+  `AUTH_STATUS` char(1) NOT NULL,
+  PRIMARY KEY (`id_AUTH`,`USER_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_auth`
+--
+
+LOCK TABLES `t_auth` WRITE;
+/*!40000 ALTER TABLE `t_auth` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_auth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_auth_biz`
+--
+
+DROP TABLE IF EXISTS `t_auth_biz`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_auth_biz` (
+  `id_AUTH_BIZ` int(11) NOT NULL AUTO_INCREMENT,
+  `AUTH_ID` int(11) NOT NULL,
+  `BIZ_CODE` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_AUTH_BIZ`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_auth_biz`
+--
+
+LOCK TABLES `t_auth_biz` WRITE;
+/*!40000 ALTER TABLE `t_auth_biz` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_auth_biz` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_menu`
+--
+
+DROP TABLE IF EXISTS `t_menu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_menu` (
+  `id_MENU` char(6) NOT NULL,
+  `parent_id` char(6) DEFAULT NULL,
+  `is_end` char(1) DEFAULT NULL,
+  `seq` int(11) DEFAULT NULL,
+  `MENU_NAME` varchar(60) DEFAULT NULL,
+  `FUN_CODE` char(6) DEFAULT NULL,
+  `TARGET` varchar(20) DEFAULT NULL,
+  `MENU_DESC` varchar(120) DEFAULT NULL,
+  `URL` varchar(120) DEFAULT NULL,
+  `AUTH_BIZ_SCOPE` varchar(60) DEFAULT NULL,
+  `MENU_STATUS` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id_MENU`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_menu`
+--
+
+LOCK TABLES `t_menu` WRITE;
+/*!40000 ALTER TABLE `t_menu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_menu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_user`
+--
+
+DROP TABLE IF EXISTS `t_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_user` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(20) DEFAULT NULL,
+  `password` varchar(45) DEFAULT NULL,
+  `user_type` char(1) DEFAULT NULL,
+  `USER_STATUS` char(1) DEFAULT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_user`
+--
+
+LOCK TABLES `t_user` WRITE;
+/*!40000 ALTER TABLE `t_user` DISABLE KEYS */;
+INSERT INTO `t_user` VALUES (1,'cja','111111','1','1');
+/*!40000 ALTER TABLE `t_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-16 15:56:17
